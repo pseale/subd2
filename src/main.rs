aa859a294ef6bff52f832cf1f799185ecef57b37
Peter Seale <peter2pseale.com>
2023-04-14 05:43:37 -0500

    follow rust naming standard

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,18 @@ struct TwitchChatMessage {
     message: String,
     user: String,
     timestamp: SystemTime,
-    timestampString: String
+    timestamp_string: String
 }
 
 fn main() {
     let timestamp = SystemTime::now();
-    let dateTimeUtc : DateTime<Utc> = timestamp.into();
-    let timestampString = dateTimeUtc.to_rfc3339();
+    let datetime_utc : DateTime<Utc> = timestamp.into();
+    let timestamp_string = datetime_utc.to_rfc3339();
     let a = TwitchChatMessage {
         message : "hey".to_string(),
         user : "jtvuser00000".to_string(),
         timestamp : timestamp,
-        timestampString : timestampString
+        timestamp_string : timestamp_string
     };
     println!("env var is: {}", dotenv::var("TEMP").unwrap());
     println!("{}", serde_json::to_string(&a).unwrap());
