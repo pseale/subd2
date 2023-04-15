cecc5613328fcc183c3b02cc545feb7441c10b36
Peter Seale <peter2pseale.com>
2023-04-15 16:42:02 -0500

    WIP - compiles now

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,58 @@
-use twitch_api::helix::HelixClient;
-use twitch_api::twitch_oauth2::{AccessToken, UserToken};
 use reqwest::Client as ReqwestClient;
-use serde::{Deserialize, Serialize};
-use std::time::SystemTime;
-use chrono::{DateTime, Utc};
+use reqwest::Url;
+use tokio_tungstenite::tungstenite;
+use twitch_api::helix::HelixClient;
+use twitch_api::tmi::UserId;
+use twitch_api::twitch_oauth2::{AccessToken, UserToken};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct TwitchChatMessage {
-    message: String,
-    user: String,
-    timestamp: SystemTime,
-    timestamp_string: String
+async fn open_websocket(
+    oauth_token: String,
+    client: HelixClient<'static, ReqwestClient>,
+    broadcaster_id: u64,
+    connect_url: Url,
+) -> Result<
+    tokio_tungstenite::WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>,
+    eyre::Error,
+> {
+    unimplemented!();
+    //       let config = tungstenite::protocol::WebSocketConfig {
+    //             max_send_queue: None,
+    //             max_message_size: Some(64 << 20), // 64 MiB
+    //             max_frame_size: Some(16 << 20),   // 16 MiB
+    //             accept_unmasked_frames: false,
+    //         };
+    //         let (socket, _) =
+    //             tokio_tungstenite::connect_async_with_config(&self.connect_url, Some(config))
+    //                 .await
+    //                 .context("Can't connect")?;
+    //
+    //         Ok(socket)
 }
-
 #[tokio::main]
 async fn main() {
-    let timestamp = SystemTime::now();
-    let datetime_utc : DateTime<Utc> = timestamp.into();
-    let timestamp_string = datetime_utc.to_rfc3339();
-    let a = TwitchChatMessage {
-        message : "hey".to_string(),
-        user : "jtvuser00000".to_string(),
-        timestamp : timestamp,
-        timestamp_string : timestamp_string
-    };
-    println!("{}", serde_json::to_string(&a).unwrap());
-    println!("-------------------------------------");
-
     let oauth_token = dotenv::var("SUBD2_OAUTH_TOKEN").unwrap();
-    let client: HelixClient<ReqwestClient> =  HelixClient::default();
+    let client: HelixClient<ReqwestClient> = HelixClient::default();
     match UserToken::from_existing(
         &client,
-        AccessToken::new(oauth_token),
+        AccessToken::new(oauth_token.clone()),
         None, // Refresh Token
         None, // Client Secret
-    ).await {
+    )
+    .await
+    {
         Ok(response) => {
             let token = response;
-            if let Ok(channel) = client.get_channel_from_login(&dotenv::var("SUBD2_USERNAME").unwrap(), &token).await {
+            if let Ok(Some(channel)) = client
+                .get_channel_from_login(&dotenv::var("SUBD2_USERNAME").unwrap(), &token)
+                .await
+            {
                 println!("Channel: {:?}", channel);
+
+                let connect_url = twitch_api::TWITCH_EVENTSUB_WEBSOCKET_URL.clone();
+                let broadcaster_id = 792462754;
+                let socket = open_websocket(oauth_token, client, broadcaster_id, connect_url);
             }
-        },
-        Err(err) => panic!("{}",err)
+        }
+        Err(err) => panic!("{}", err),
     }
 }
